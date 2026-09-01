31c4b49736dae06b043ea670610151e0bcc4c963
Damien Castello <d.castello.13200@gmail.com>
2026-09-01 11:38:47 +0200

    tp4&5: done

--- src/main/java/fr/castello/entity/Ville.java
+++ src/main/java/fr/castello/entity/Ville.java
@@ -1,12 +1,22 @@
 package fr.castello.entity;
 
+import java.util.Objects;
+
 public class Ville {
+    private long id;
     private String nom;
     private int population;
 
+    private static int compteur = 1;
+
     public Ville(String nom, int population) {
+        this.id = compteur++;
         this.nom = nom;
         this.population = population;
+    }
+
+    public long getId() {
+        return id;
     }
 
     public int getPopulation() {
@@ -23,5 +33,25 @@ public class Ville {
 
     public void setNom(String nom) {
         this.nom = nom;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Ville ville = (Ville) o;
+        return id == ville.id && population == ville.population && Objects.equals(nom, ville.nom);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nom, population);
+    }
+
+    @Override
+    public String toString() {
+        return "Ville{" +
+                "nom='" + nom + '\'' +
+                ", population=" + population +
+                '}';
     }
 }
